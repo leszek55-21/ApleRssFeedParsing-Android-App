76c3b326e3add2bc8bafcb3a118f7f5c686c53e3
Leszek <leszek55-21@tlen.pl>
2018-12-12 22:59:30 +0100

    initial

---
+++ src/main/java/com/leszekszymaszek/toptensongs/FeedAdapter.java
@@ -0,0 +1,63 @@
+package com.leszekszymaszek.toptensongs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FeedAdapter<T extends FeedEntry> extends ArrayAdapter {
+
+    private static final String TAG = "FeedAdapter";
+
+    private final int layotResource;
+    private final LayoutInflater layoutInflater;
+    private List<T> applications;
+
+    public FeedAdapter(Context context, int resource, List<T> applications) {
+        super(context, resource);
+        this.layotResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            convertView = layoutInflater.inflate(layotResource, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        T currentApp = applications.get(position);
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View view) {
+            this.tvName = view.findViewById(R.id.tvName);
+            this.tvArtist = view.findViewById(R.id.tvArtist);
+            this.tvSummary = view.findViewById(R.id.tvSummary);
+        }
+    }
+}
